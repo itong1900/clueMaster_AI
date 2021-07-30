21acf1f6fe56c0ae05b85f28a77d27fcbece08c4
Yitong Chen <itong1900@gmail.com>
2021-07-30 16:06:06 -0700

    add unit testing unit

---
+++ src/test/first_module.py
@@ -0,0 +1,8 @@
+
+# def hi():
+#     print("helloWorld")
+
+# if __name__ == '__main__':
+#     hi()
+
+print(__name__)
--- src/test/test_player.py
+++ src/test/test_player.py
@@ -1,12 +1,25 @@
+
+## import first_module
+
+#print("first_module's __name__: {}".format(first_module.__name__))
+# print("second MOdule's name: {}".format(__name__))
+
+
 import unittest
-from clueMaster_AI.src.main.Player import Player
+import sys
+sys.path.append("../main/")
+from Player import Player
+
 
 class TestPlayer(unittest.TestCase):
 
     def test_update_suspect_must_have(self):
-        agent = Player.Player()
+        agent = Player("Jerry", 6)
         agent.update_suspect_must_have("Miss Peach")
         self.assertEqual(agent.suspect_must_have, {"Miss Peach"})
 
-if __name == '__main__':
+if __name__ == '__main__':
     unittest.main()
+
+
+
