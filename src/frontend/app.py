64c4bf9027783a29c352ff8772c3a94c18ca2f7b
Yitong Chen <itong1900@gmail.com>
2021-09-25 22:38:21 -0700

    add main framework to the app.py

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -14,53 +14,61 @@ sys.path.append("../main/")
 from Player import Player
 from Player import Secret
 
-st.title("""
-Welcome to the Game of Clue 
-""")
 
-st.sidebar.title(
-    "Game Configuration"
-)
+def main():
+    st.title("""Welcome to the Game of Clue """)
+    game_mode = st.radio('START HERE: Select Game Mode', ["Advisor", "Simulator(not Deployed)"])
 
-st.sidebar.text(
-    "START HERE(only when in Advisor Mode)"
-)
+    if game_mode == "Advisor":    
+        st.sidebar.title("Game Configuration")
+        st.sidebar.caption("Advisor Mode")
 
-number_of_player = st.sidebar.number_input(
-    "Enter number of player:",
-    min_value = 2, max_value = 20, value = 4, step=1
-)
+        number_of_player = st.sidebar.number_input(
+            "Enter number of player:",
+            min_value = 2, max_value = 20, value = 4, step=1
+        )
 
+        suspect_myself_have = st.sidebar.multiselect("Suspect card(s) in your hand", LIST_SUSPECT)
+        weapon_myself_have = st.sidebar.multiselect("Weapon card(s) in your hand", LIST_WEAPON)
+        room_myself_have = st.sidebar.multiselect("Room card(s) in your hand", LIST_ROOM)
 
-## TODO: add input validation
-suspect_myself_have = st.sidebar.text_area(
-    "Enter the suspect card in your hand, seperated by ','"
-)
+        # Enter name and number of cards of your opponents.
+        inputs = []
+        for i in range(1, number_of_player):
+            input = st.sidebar.text_area("Enter the name of the opponent_" + str(i) + " and number of card he/she has, i.e Mia, 6")
+            inputs.append(input)
 
+        st.sidebar.text("Don't modify this part after game start, and recommend hide this part while playing")
 
-## TODO: add input validation
-weapon_myself_have = st.sidebar.text_area(
-    "Enter the weapon card in your hand, seperated by ','"
-)
+        st.text('Click start the game when finishing editting configuration')
+        if st.button('Start the game'):
+            advisor_mode(inputs, suspect_myself_have, weapon_myself_have, room_myself_have, number_of_player)
 
-## TODO: add input validation
-room_myself_have = st.sidebar.text_area(
-    "Enter the room card in your hand, seperated by ','"
-)
-
-## Enter name and number of cards of your opponents.
-# opponent_list = []
-# for i in range(number_of_player):
+    else:
+        pass
 
 
+def advisor_mode(opponents_info, suspect_myself_have, weapon_myself_have, room_myself_have, number_of_player):
+    #parse input 
+    opponent_list_hashmap = {}
+    # for single_input in opponents_info:
+    #     name, card_amount = single_input.split(",")[0].strip(), single_input.split(",")[1].strip()
+    #     opponent_list_hashmap[name] = int(card_amount)
 
-st.sidebar.text(
-    "Don't modify this part after game start, and recommend hide this part while playing"
-)
+    st.checkbox('Show Log')
+    st.checkbox('Show suggestion')
 
-game_mode = st.radio('Select Game Mode', ["Advisor", "Simulator"])
+if __name__ == '__main__':
+    main()
 
-st.button('Start the game')
 
-st.selectbox('Select', [1,2,3])
-st.select_slider('Slide to select', options=[1,'2'])
+
+
+
+
+
+
+
+# st.selectbox('Select', [1,2,3])
+# st.select_slider('Slide to select', options=[1,'2'])
+
